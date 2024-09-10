de658c5d72d47530229ff0e2cb07daf9f147ee10
ethanshealey <ethan.shealey@gmail.com>
2024-09-10 09:34:08 -0400

    add url to photo site

--- src/components/CameraWindowContent.tsx
+++ src/components/CameraWindowContent.tsx
@@ -1,7 +1,7 @@
 import cameraJson from "@/data/cameras.json"
 import GenericEntryType from "@/types/GenericEntryType"
 import GenericEntry from "./GenericEntry"
-import { ScrollView, Separator } from "react95"
+import { Anchor, ScrollView, Separator } from "react95"
 import sort from "@/util/sort"
 import getMinYear from "@/util/getMinYear"
 import getMaxYear from "@/util/getMaxYear"
@@ -11,7 +11,7 @@ const CameraWindowContent = () => {
   return (
     <ScrollView className='scroll-view'>
       <h1>Cameras</h1>
-      <p>Antique cameras have been the main focus of my dive into this rabbit hole. I currently have {cameraJson.length} cameras, originating from between {getMinYear(cameraJson)} and {getMaxYear(cameraJson)}</p>
+      <p>Antique cameras have been the main focus of my dive into this rabbit hole. I currently have {cameraJson.length} cameras, originating from between {getMinYear(cameraJson)} and {getMaxYear(cameraJson)}.<br/><br/>To view some of the pictures I have taken with these cameras, check out <Anchor href="https://photos.ethanshealey.com/">my photography website!</Anchor></p>
       <Separator />
       {
         sort(cameraJson).map((camera: GenericEntryType, idx: number) => (
